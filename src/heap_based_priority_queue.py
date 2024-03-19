386708e9e478ff08a6252ae80e5fd88600845054
serafindanyil <danyil.serafin.ir.2023@lpnu.ua>
2024-03-19 12:24:21 +0200

    Fix
    Bug fix

--- src/heap_based_priority_queue.py
+++ src/heap_based_priority_queue.py
@@ -15,11 +15,74 @@
 
 
 class Node:
-    def __init__(self, value, priority):
+    def __init__(self, value, priority=0):
         self.value = value
         self.priority = priority
 
-    
+
+class PriorityQueue:
+    HEAP = [] # Відсортована бінарна хіпа
+
+    def up(self, i):
+        while i != 0 and self.HEAP[i].priority > self.HEAP[(i - 1) // 2].priority:
+            self.HEAP[i], self.HEAP[(i - 1) // 2] = self.HEAP[(i - 1) // 2], self.HEAP[i]
+            i = (i - 1) // 2
+
+    def down(self, i):
+        n = len(self.HEAP)
+        while 2 * i + 1 < n:
+            max_child = 2 * i + 1
+            if max_child + 1 < n and self.HEAP[max_child].priority < self.HEAP[max_child + 1].priority:
+                max_child += 1
+            if self.HEAP[i].priority >= self.HEAP[max_child].priority:
+                break
+            self.HEAP[i], self.HEAP[max_child] = self.HEAP[max_child], self.HEAP[i]
+            i = max_child
+
+    def insert_element(self, obj):
+        self.HEAP.append(obj)
+        self.up(len(self.HEAP) - 1)
+
+    def delete_element(self, obj):
+        obj_index = None
+        n = len(self.HEAP)
+        for index in range(0,n):
+            if obj == self.HEAP[index]:
+                obj_index = index
+        self.HEAP[obj_index], self.HEAP[n-1] = self.HEAP[n-1], self.HEAP[obj_index]
+        self.HEAP.pop(n-1)
+        self.down(obj_index)
+
+    def show_priority_queue_with_change(self):
+        for node in self.HEAP:
+            print(f"Value: {node.value}, Priority: {node.priority}")
+
+
+pq = PriorityQueue()
+
+danik = Node("Danik", 300)
+pq.insert_element(danik)
+
+andrew = Node("Andrew", 20)
+pq.insert_element(andrew)
+
+kolya = Node("Kolya", 22)
+pq.insert_element(kolya)
+
+sergey = Node("Sergey", 31)
+pq.insert_element(sergey)
+
+igor = Node("Igor", 21)
+pq.insert_element(igor)
+
+nazar = Node("Nazar", 322)
+pq.insert_element(nazar)
+
+vasya = Node("Vasya", 400)
+pq.insert_element(vasya)
+
+pq.delete_element(vasya)
+pq.show_priority_queue_with_change()
 
 
 

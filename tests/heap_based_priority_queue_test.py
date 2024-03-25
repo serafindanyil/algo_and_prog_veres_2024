f4db138fa3239d451319901c3e83017e4f157c9f
serafindanyil <danyil.serafin.ir.2023@lpnu.ua>
2024-03-25 11:49:33 +0200

    Small fix and rename, delete less files

--- tests/heap_based_priority_queue_test.py
+++ tests/heap_based_priority_queue_test.py
@@ -3,18 +3,20 @@ Unittest function 'Node' and 'PriorityQueue'
 """
 
 from unittest import TestCase, main
-from src.heap_based_priority_queue.heap_based_priority_queue import Node, PriorityQueue
+from src.heap_based_priority_queue import Node, PriorityQueue
 
 
 class FindBiggestElementTest(TestCase):
     def test_insert_element(self):
         pq = PriorityQueue()
+
         test = Node("test", 100)
         pq.insert_element(test)
         self.assertEqual(len(pq.heap), 1)
 
     def test_delete_element(self):
         pq = PriorityQueue()
+
         should_delete = Node("test", 100)
         pq.insert_element(should_delete)
         test1 = Node("test1", 10)
@@ -25,6 +27,22 @@ class FindBiggestElementTest(TestCase):
         pq.insert_element(test3)
         pq.delete_element(should_delete)
         self.assertEqual(len(pq.heap), 3)
+
+    def test_correct_priority_queue(self):
+        pq = PriorityQueue()
+
+        danik = Node("Danik", 300)
+        pq.insert_element(danik)
+
+        andrew = Node("Andrew", 20)
+        pq.insert_element(andrew)
+
+        vasya = Node("Vasya", 400)
+        pq.insert_element(vasya)
+
+        result = [node.priority for node in pq.heap]
+        self.assertEqual(result, [400, 20, 300])
+
 
 if __name__ == '__main__':
     main()

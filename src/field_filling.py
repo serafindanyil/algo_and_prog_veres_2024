f17125a0d0b3a572c3e98063fbe4fba202ecd296
serafindanyil <danyil.serafin.ir.2023@lpnu.ua>
2024-04-09 14:14:26 +0300

    add new code

--- src/field_filling.py
+++ src/field_filling.py
@@ -1,67 +1,49 @@
-def __read_input_data_from_file(input_file):
-    file = open(f'../src/field_filling_resources/{input_file}', 'r')
+def read_input_data_from_file(input_file):
+    if input_file is None:
+        raise FileNotFoundError("File not found")
     try:
-        rows, colunms = tuple(map(int, file.readline().split(',')))
-        start_pos = tuple(map(int, file.readline().split(',')))
-        change_color = str(eval(file.readline()))
-        matrix = [eval(file.readline().strip(',\n')) for _ in range(rows)]
+        with open(f'../src/field_filling_resources/{input_file}', 'r') as file:
+            rows, colunms = tuple(map(int, file.readline().split(',')))
+            start_pos = tuple(map(int, file.readline().split(',')))
+            change_color = str(eval(file.readline()))
+            matrix = [eval(file.readline().strip(',\n')) for cycle in range(rows)]
     except FileNotFoundError:
-        FileNotFoundError('File not found')
-    file.close()
+        raise FileNotFoundError(f"File '{input_file}' not found")
+    except ValueError:
+        raise ValueError('Value is not corrected')
+    finally:
+        file.close()
     return rows, colunms, start_pos, change_color, matrix
 
 
-def __write_output_data_to_file(output_file, matrix):
+def write_output_data_to_file(output_file, matrix):
     file = open(f'../src/field_filling_resources/{output_file}', 'w')
     for rows in matrix:
         file.write(f'{rows}\n')
 
 
 def field_filling(input_file, output_file):
-    rows, colunms, start_pos, change_color, matrix = __read_input_data_from_file(input_file)
+    rows, colunms, start_pos, change_color, matrix = read_input_data_from_file(input_file)
     start_color = matrix[start_pos[0]][start_pos[1]]
 
-    visited = [[False] * colunms for _ in range(rows)]
+    try:
+        if start_color is change_color:
+            raise ValueError
+    except ValueError:
+        raise ValueError('The color it replaces is the same')
+
+    visited = []
     queue = [(start_pos[0], start_pos[1])]
 
     while queue:
         row, col = queue.pop(0)
-        if row < 0 or row >= rows or col < 0 or col >= colunms or visited[row][col] or matrix[row][col] != start_color:
+        if row < 0 or row >= rows or col < 0 or col >= colunms or (row, col) in visited or matrix[row][col] != start_color:
             continue
         matrix[row][col] = change_color
-        visited[row][col] = True
+        visited.append((row, col))
         queue.append((row + 1, col))
         queue.append((row - 1, col))
         queue.append((row, col + 1))
         queue.append((row, col - 1))
-    __write_output_data_to_file(output_file, matrix)
-    # while queue:
-    #     row, col = queue.pop(0)
-    #     if (row < 0 or row >= rows or col < 0 or col >= col or visited[row][col] or matrix[row][col] != start_color):
-    #         continue
-    #     matrix[row][col] = replacement_color
-    #     visited[row][col] = True
-    #     queue.append((row + 1, col))
-    #     queue.append((row - 1, col))
-    #     queue.append((row, col + 1))
-    #     queue.append((row, col - 1))
-    # # def bfs(visited, graph, node):  # function for BFS
-    #     visited.append(mtrx(node))
-    #     queue.append(mtrx(node))
-    #
-    #
-    #     while queue:  # Creating loop to visit each node
-    #         row, col = queue.pop(0)
-    #         for neighbour in graph[m]:
-    #             if neighbour not in visited:
-    #                 visited.append(neighbour)
-    #                 queue.append(neighbour)
-    #
-    # bfs(visited, matrix, start_pos)
-    print(start_color)
-    print(change_color)
-    print(type(start_color))
-    print(type(change_color))
-
-
-field_filling(input_file='input_data.txt', output_file='output_data.txt')
+        return matrix
+    write_output_data_to_file(output_file, matrix)
---
+++ tests/field_filling_test.py
@@ -0,0 +1,29 @@
+from unittest import TestCase, main
+from src.field_filling import field_filling, read_input_data_from_file, write_output_data_to_file
+
+
+class FieldFillingTest(TestCase):
+    def test_field_filling(self):
+        field_filling(input_file='test_input.txt', output_file='test_output')
+        test_input = open(f'../src/field_filling_resources/test_input.txt', 'r')
+        test_field_filling = open(f'../src/field_filling_resources/test_field_filling.txt', 'r')
+        self.assertTrue(test_input, test_field_filling)
+
+    def test_none_input_data(self):
+        with self.assertRaises(FileNotFoundError):
+            read_input_data_from_file(input_file=None)
+
+    def test_write_output_data_to_file(self):
+        test_matrix = [['G', 'G', 'G'], ['G', 'G', 'G'], ['G', 'G', 'G']]
+        write_output_data_to_file(output_file='test_output', matrix=test_matrix)
+        test_write_output = open(f'../src/field_filling_resources/test_output.txt', 'r')
+        test_correct_output = open(f'../src/field_filling_resources/test_wrrite_output_data_to_file_correct.txt', 'r')
+        self.assertTrue(test_write_output, test_correct_output)
+
+    def test_same_color(self):
+        with self.assertRaises(ValueError):
+            field_filling(input_file='test_same_color_input.txt', output_file='test_same_color_output')
+
+
+if __name__ == '__main__':
+    main()
